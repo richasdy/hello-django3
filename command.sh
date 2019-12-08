13a9d043c1d001d69c584c4e3c9c62d274b98898
richasdy <donni.richasdy@gmail.com>
2019-12-08 07:27:06 +0700

    run dev server

--- command.sh
+++ command.sh
@@ -8,3 +8,6 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 django-admin startproject django3 .
+python manage.py runserver
+http://127.0.0.1:8000
+http://127.0.0.1:8000/admin
