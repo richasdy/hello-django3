24e37e845bf3a0a7d072c3fe157f8edc54a12c70
richasdy <donni.richasdy@gmail.com>
2019-12-08 20:52:03 +0700

    tutorial5

--- command.sh
+++ command.sh
@@ -25,3 +25,40 @@ python manage.py makemigrations polls
 python manage.py sqlmigrate polls 0001
 python manage.py migrate
 python manage.py shell
+# -------- model -------- #
+# from polls.models import Choice, Question
+# Question.objects.all()
+# Question.objects.filter(id=1)
+# Question.objects.filter(question_text__startswith='What')
+# from django.utils import timezone
+# current_year = timezone.now().year
+# Question.objects.get(pub_date__year=current_year)
+# Question.objects.get(id=2)
+# Question.objects.get(pk=1)
+# q = Question.objects.get(pk=1)
+# q.was_published_recently()
+# q = Question.objects.get(pk=1)
+# q.choice_set.all()
+# q.choice_set.create(choice_text='Not much', votes=0)
+# q.choice_set.create(choice_text='The sky', votes=0)
+# c = q.choice_set.create(choice_text='Just hacking again', votes=0)
+# c.question
+# q.choice_set.all()
+# q.choice_set.count()
+# Choice.objects.filter(question__pub_date__year=current_year)
+# c = q.choice_set.filter(choice_text__startswith='Just hacking')
+# c.delete()
+# -------- model -------- #
+python manage.py test polls
+python manage.py shell
+# -------- test -------- #
+# from django.test import Client
+# client = Client()
+# response = client.get('/')
+# response.status_code
+# from django.urls import reverse
+# response = client.get(reverse('polls:index'))
+# response.status_code
+# response.content
+# response.context['latest_question_list']
+# -------- test -------- #
--- polls/views.py
+++ polls/views.py
@@ -9,6 +9,7 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
@@ -36,8 +37,11 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 # def detail(request, question_id):
 #     return HttpResponse("You're looking at question %s." % question_id)
@@ -56,6 +60,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # def results(request, question_id):
 #     response = "You're looking at the results of question %s."
