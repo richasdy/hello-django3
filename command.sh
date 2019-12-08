904590da87cc879a7b5141229992fc3c4ab9e9c4
richasdy <donni.richasdy@gmail.com>
2019-12-08 07:29:51 +0700

    migrate + create admin

--- command.sh
+++ command.sh
@@ -9,5 +9,8 @@ source venv/bin/activate
 pip install -r requirements.txt
 django-admin startproject django3 .
 python manage.py runserver
-http://127.0.0.1:8000
-http://127.0.0.1:8000/admin
+# http://127.0.0.1:8000
+# http://127.0.0.1:8000/admin
+python manage.py migrate
+python manage.py createsuperuser
+# http://127.0.0.1:8000/admin login admin/admin
